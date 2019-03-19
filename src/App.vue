c8a8ef982ffcb7a1e12d481defb34ee4a3388ab1
Kuang-Cheng, Chien <me@kcchien.com>
2019-03-19 22:09:19 +0800

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Template </span>
+        <span class="font-weight-light">GENERATOR ENGINE</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      //
+    };
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,166 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome
+        </h1>
+        <p class="subheading font-weight-regular">
+          Demo proto type template here.
+        </p>
+      </v-flex>
+
+      <v-flex mb-5 xs12>
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <p class="subheading font-weight-regular">
+          Give me some feedback!
+        </p>
+      </v-flex>
+
+      <v-flex xs12 sm6 d-flex>
+        <v-select :items="items" label="Finding"></v-select>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-combobox
+          v-model="select"
+          :items="combo_items"
+          label="Select an amount or create a new one"
+        ></v-combobox>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-combobox
+          v-model="select"
+          :items="take_notes_items"
+          label="Take notes of"
+        ></v-combobox>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-combobox
+          v-model="select"
+          :items="segment_items"
+          label="Select a location segment"
+        ></v-combobox>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-combobox
+          v-model="select"
+          :items="orientation_items"
+          label="Select a location orientation"
+        ></v-combobox>
+      </v-flex>
+
+      <v-flex xs12 sm6 md3>
+        <v-text-field label="Size (cm)"></v-text-field>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-combobox
+          v-model="select"
+          :items="intervention_items"
+          label="Select an intervention item"
+        ></v-combobox>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-textarea
+          outline
+          name="input-7-4"
+          label="Generated text"
+          v-model="result_text"
+        ></v-textarea>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data: () => ({
+    items: ["Polyp", "Lymphoid hyperplasia-terminal ileum"],
+    combo_items: ["1", "several", "multiple"],
+    take_notes_items: ["the most significant", "each", "overall"],
+    segment_items: [
+      "Terminal ileum",
+      "Cecum",
+      "Ascending colon",
+      "Hepatic flexure",
+      "Transverse colon",
+      "Splenic flexure",
+      "Descending colon",
+      "Sigmoid colon",
+      "RS junction",
+      "Rectum",
+      "All segments of colon",
+      "Anastomosis"
+    ],
+    orientation_items: [
+      "<none>",
+      "proximal",
+      "middle",
+      "distal",
+      "upper",
+      "lower"
+    ],
+    intervention_items: ["Biopsy", "Polypectomy", "EMR", "ESD"],
+    result_text:
+      "(83) (87)colon  polyp/polyps(視83單複數), (86)(可以是range), (88)，(89-t), (90-t), (91-t),  at the (85)location(可多選) was/were found. s/p (92)",
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader"
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify"
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify"
+      }
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com"
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com"
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuetifyjs.com"
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs"
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify"
+      }
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ]
+  })
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
